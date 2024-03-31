8e1f810694c85a33840bf9a749c2408855d86cdb
DanielaBadea <badeadaniella@gmail.com>
2024-03-31 06:01:07 +0300

    finish task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import './App.css';
+import Profile from './Profile/Profile';
+import user from './Profile/data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+ <>
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+ </>
   );
 };
