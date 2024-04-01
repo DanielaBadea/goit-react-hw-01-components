14b5c588a3afd5baeb58eec8bf02dae00584750d
DanielaBadea <badeadaniella@gmail.com>
2024-04-01 05:28:30 +0300

    task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,24 @@ import user from './Profile/data/user.json';
 import data from './Statistics/data/data.json';
 import FriendList from './Friends/FriendList';
 import friends from './Friends/data/friends.json';
+import TransactionHistory from './Transaction/TransactionHistory';
+import transactions from './Transaction/data/transactions.json'
 
 export const App = () => {
   return (
  <>
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
-<Statistics 
-title="Upload stats" stats={data}
- />
- <FriendList friends={friends} />
+    <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data}/>
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />;
+
  </>
+ 
   );
 };
